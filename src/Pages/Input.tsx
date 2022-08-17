305e6c0c324b12fa3ea2b269e6dfc23a5aa9a2c4
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2022-08-17 01:37:59 -0300

    removing redux

--- src/Pages/Input.tsx
+++ src/Pages/Input.tsx
@@ -1,10 +1,6 @@
 import { faMapMarkerAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { getCityInfo } from "../redux/cityInfoSlice";
-import { setForecast } from "../redux/currentConditionsSlice";
-
 
 const Input: React.FC = () => {
     const [cityName, setCityName] = useState('');
@@ -17,8 +13,7 @@ const Input: React.FC = () => {
         value={cityName} name="cityName" onChange={e => setCityName(e.target.value)}/>
 
         <FontAwesomeIcon icon={faSearch} className="input-icon" type="submit" 
-        onClick={() => useDispatch()
-        // useDispatch(setForecast(getCityInfo(cityName).Key))
+        onClick={() => console.log('chego aqui')
         }/>
     </div>
 }
